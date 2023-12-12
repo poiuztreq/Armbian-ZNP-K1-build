9964bc5446ad069992517b862196a96f5b33b96f
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2023-12-12 21:49:05 +0000

    Update customize-image.sh

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -2,90 +2,55 @@
 
 # NOTE: If you want to transfer files between chroot and host
 # userpatches/overlay directory on host is bind-mounted to /tmp/overlay in chroot
-# The sd card's root path is accessible via $SDCARD variable.
+# The SD card's root path is accessible via $SDCARD variable.
 
-RELEASE=$1
-LINUXFAMILY=$2
-BOARD=$3
-BUILD_DESKTOP=$4
-
-Main() {
-    InstallNeptune4Setup
-    esac
-} # Main
-
-InstallNeptune4Setup()
-{
-    # Update package list
-    apt-get update
-
-    # Install ustreamer, git, and Python packages
-    apt-get install -y ustreamer git python3-numpy python3-matplotlib libatlas-base-dev
-
-    # Add user 'mks'
-    useradd -m -G sudo -s /bin/bash mks
-
-    # Set password for both 'mks' and 'root' to 'makerbase'
-    echo 'mks:makerbase' | chpasswd
-    echo 'root:makerbase' | chpasswd
-
-    # Add 'mks' to 'gpio' group, create group if it doesn't exist
-    groupadd gpio || true
-    usermod -a -G gpio mks
-
-    # Add 'mks' to 'spiusers' group, create group if it doesn't exist
-    groupadd spiusers || true
-    usermod -a -G spiusers mks
-
-    # GPIO script setup
-    SetupGpioScript
-
-    # Add cron job to run sync command every 10 minutes
-    SetupCronJob
-
-    # Clone OpenNept4une repository
-    CloneOpenNept4une
-} # Installx
-
-SetupGpioScript()
-{
-    # Path of the script to be created
-    SCRIPT_PATH="/usr/local/bin/set_gpio.sh"
-
-    # Create and write the GPIO command to the script
-    echo -e "#!/bin/bash\n/usr/bin/gpioset gpiochip1 14=0; /usr/bin/gpioset gpiochip1 15=0; sleep 0.5; /usr/bin/gpioset gpiochip1 15=1" | sudo tee "$SCRIPT_PATH" >/dev/null
-
-    # Make the script executable
-    sudo chmod +x "$SCRIPT_PATH"
-
-    # Check if /etc/rc.local exists
-    RC_LOCAL="/etc/rc.local"
-    if [ ! -f "$RC_LOCAL" ]; then
-        # Create /etc/rc.local if it doesn't exist
-        echo "#!/bin/bash" | sudo tee "$RC_LOCAL" >/dev/null
-        echo "exit 0" | sudo tee -a "$RC_LOCAL" >/dev/null
-        sudo chmod +x "$RC_LOCAL"
-    fi
-
-    # Insert the script path before 'exit 0' in /etc/rc.local
-    sudo sed -i "/^exit 0/i $SCRIPT_PATH" "$RC_LOCAL"
+# Function to handle errors
+handle_error() {
+    echo "Error occurred in script at line: $1"
+    exit 1
 }
 
-SetupCronJob()
-{
-    # Add cron job to run sync command every 10 minutes
-    CRON_ENTRY="*/10 * * * * /bin/sync"
-    if ! (crontab -l 2>/dev/null | grep -F "/bin/sync"); then
-        (crontab -l 2>/dev/null; echo "$CRON_ENTRY") | crontab -
-        echo "Sync command added to the crontab to run every 10 minutes."
-    else
-        echo "The sync command is already in the crontab."
-    fi
-}
+# Trap errors
+trap 'handle_error $LINENO' ERR
 
-CloneOpenNept4une()
-{
-    su - mks -c "git clone https://github.com/halfmanbear/OpenNept4une.git /home/mks/OpenNept4une"
-}
+# Copy DTB files to the boot partition
+cp /tmp/overlay/boot/dtb/rockchip/*.dtb $SDCARD/boot/dtb/rockchip/
 
-Main "$@"
+# Copy rules files to the rules.d folder
+cp /tmp/overlay/etc/udev/rules.d/*.rules $SDCARD/etc/udev/rules.d/
+
+# Update package list and install packages
+apt-get update
+apt-get install -y ustreamer git python3-numpy python3-matplotlib libatlas-base-dev
+
+# Add user 'mks'
+useradd -m -G sudo -s /bin/bash mks || true
+
+# Set password for both 'mks' and 'root' to 'makerbase'
+echo 'mks:makerbase' | chpasswd
+echo 'root:makerbase' | chpasswd
+
+# Add 'mks' to 'gpio' and 'spiusers' groups, create groups if they don't exist
+groupadd gpio || true
+groupadd spiusers || true
+usermod -a -G gpio,spiusers mks
+
+# Create and configure GPIO script
+SCRIPT_PATH="/usr/local/bin/set_gpio.sh"
+echo -e "#!/bin/bash\n/usr/bin/gpioset gpiochip1 14=0; /usr/bin/gpioset gpiochip1 15=0; sleep 0.5; /usr/bin/gpioset gpiochip1 15=1" > "$SCRIPT_PATH"
+chmod +x "$SCRIPT_PATH"
+
+# Configure /etc/rc.local for startup script execution
+RC_LOCAL="/etc/rc.local"
+if [ ! -f "$RC_LOCAL" ]; then
+    echo -e "#!/bin/bash\n\nexit 0" > "$RC_LOCAL"
+    chmod +x "$RC_LOCAL"
+fi
+sed -i "/^exit 0/i $SCRIPT_PATH" "$RC_LOCAL"
+
+# Add cron job to run sync command every 10 minutes
+CRON_ENTRY="*/10 * * * * /bin/sync"
+(crontab -l 2>/dev/null | grep -qF "$CRON_ENTRY") || (crontab -l 2>/dev/null; echo "$CRON_ENTRY") | crontab -
+
+# Clone Git repository as user 'mks'
+su - mks -c "git clone https://github.com/halfmanbear/OpenNept4une.git /home/mks/OpenNept4une"
